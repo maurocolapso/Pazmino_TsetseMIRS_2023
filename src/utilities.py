a1c1e0f31214fe3f739ee3e9b2a3cecf9f45c6ed
Mauro <pazminomaurov@gmail.com>
2023-06-08 18:35:25 +0100

    nested

--- src/utilities.py
+++ src/utilities.py
@@ -1,10 +1,18 @@
 import pandas as pd
+import numpy as np
+from matplotlib.ticker import FormatStrFormatter
+import seaborn as sn
 
 # preprocessing
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
+from sklearn.metrics import RocCurveDisplay
+from sklearn.metrics import auc
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score
 
 # models
 from sklearn.linear_model import LogisticRegression
@@ -78,3 +86,162 @@ def test_model(X_hd_train, X_hd_test, y_hd_train):
     y_hd_pred = pipe.predict(X_hd_test)
 
     return y_hd_pred, pipe
+
+
+# nested cross validation
+
+def nested_crossvalidation(X, y):
+    """"Perform nested cross validation on a classifier
+    Parameters
+    ----------
+    X: dataframe
+    y:
+    Return
+    ------
+     cm_nested: 
+     y_pred_nested: 
+     y_test_nested:
+    """
+
+    scaler = StandardScaler()
+    model = LogisticRegression(penalty='l2', max_iter=10000)
+
+    pipe = Pipeline(steps=[("scaler", scaler), ("model", model)])
+
+    # configure nested cross-validation layers
+    cv_outer = KFold(n_splits=10, shuffle=True, random_state=123)
+    cv_inner = KFold(n_splits=5, shuffle=True, random_state=123)
+
+    # create confusion matrix list to save each of external cv layer
+    cm_nested = []
+    y_pred_nested = []
+    y_test_nested = []
+    outer_results = list()
+
+    for train_ix, test_ix in cv_outer.split(X):
+    # split data
+        X_train, X_test = X.iloc[train_ix, :], X.iloc[test_ix, :]
+        y_train, y_test = y[train_ix], y[test_ix]
+
+        # define search space
+        param_grid = {'model__C': [100, 10, 1.0, 0.1, 0.01], 'model__solver': ["newton-cg", 'lbfgs', 'liblinear']}
+        # define search
+        search = GridSearchCV(pipe, param_grid, scoring='roc_auc', cv=cv_inner, refit=True)
+        # execute search
+        result = search.fit(X_train, y_train)
+        
+        # get the best performing model fit on the whole training set
+        best_model = result.best_estimator_
+
+        # create pipeline with the best model
+        best_pipe = Pipeline(steps=[("scaler", scaler), ("best_model", best_model)])
+        best_pipe.fit(X_train, y_train)
+
+        # evaluate model on the hold out dataset
+
+        y_pred = best_pipe.predict(X_test)
+        y_desicion = best_pipe.decision_function(X_test)
+
+        # evaluate the model
+        acc = accuracy_score(y_test, y_pred)
+        cm = confusion_matrix(y_test, y_pred)
+
+        # store the result
+        outer_results.append(acc)
+        cm_nested.append(cm)
+        y_pred_nested.append(y_desicion)
+        y_test_nested.append(y_test)
+
+        print(f"Acc={(acc):.3f}, auc-roc={(result.best_score_):.3f}, cfg={result.best_params_}")
+    
+    #print('Accuracy: %.3f (%.3f)' % (np.mean(outer_results), np.std(outer_results)))
+    print(f'Mean accuracy: {np.mean(outer_results):.2f} ({np.std(outer_results):.2f})')    
+    return cm_nested, y_pred_nested, y_test_nested
+
+
+def nested_ROC_plot(y_test_nested, y_pred_nested, ax=None):
+    """
+    Plot ROC curve of each outer layer from nested crossvalidation
+    with the standard deviation.
+
+    Parameters
+    ----------
+    y_test_nested: Estimated targets as returned by a classifier.
+         Ground truth (correct) target values.
+         y_test splits used in nested cv.
+
+    y_pred_nested: Estimated targets as returned by a classifier.
+        Desicion function of the classifier.
+
+    ax: matplotlib axes, default=None
+        Axes object to plot on. If None, a new figure and axes is created.
+    
+    Return
+    ------
+     viz: RocCurveDisplay
+        Object that stores computed values
+    """
+
+    tprs = []
+    aucs = []
+    mean_fpr = np.linspace(0, 1, 100)
+    sn.set_style("ticks")
+    #sn.set_context("talk", font_scale=1.5)
+    
+    # create axis
+    ax=ax
+
+    for y_test, y_pred in zip(y_test_nested, y_pred_nested):
+
+    # ROC curve from predictions
+        viz = RocCurveDisplay.from_predictions(
+                y_test,
+                y_pred,
+                alpha=0.4,
+                lw=1,
+                #label="_",
+                ax=ax)
+
+
+        interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
+        interp_tpr[0] = 0.0
+        tprs.append(interp_tpr)
+        aucs.append(viz.roc_auc)
+
+    # Adding dumb classifier and standard deviation 
+    
+    ax.plot([0, 1], [0, 1], linestyle="--", lw=2, color="r", alpha=0.8)
+    mean_tpr = np.mean(tprs, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    std_auc = np.std(aucs)
+
+
+    ax.plot(
+        mean_fpr,
+        mean_tpr,
+        color="darkblue",
+        label=r"Mean ROC (AUC = %0.2f $\pm$ %0.2f)" % (mean_auc, std_auc),
+        lw=2.5,
+        alpha=1)
+    std_tpr = np.std(tprs, axis=0)
+    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    ax.fill_between(
+        mean_fpr,
+        tprs_lower,
+        tprs_upper,
+        color="grey",
+        alpha=0.2)
+    ax.set(
+        xlim=[-0.05, 1.05],
+        ylim=[-0.05, 1.05],
+        ylabel="True positive rate",
+        xlabel="False positive rate")
+
+
+    #ax.legend(fontsize=15)
+    ax.get_legend().remove()
+    print(f"Mean AUC = {mean_auc:.3f} ({std_auc:.3f})")
+    return viz
+
